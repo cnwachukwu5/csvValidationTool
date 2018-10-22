46f58d1d1b4963fe63821d34a4f969449b384350
Cyprian Nwachukwu <cnwachukwu5@gmail.com>
2018-10-22 16:58:39 -0400

    CVS record separator and column split app

---
+++ src/main/java/com/appslabtest/ValidationtoolApplication.java
@@ -0,0 +1,12 @@
+package com.appslabtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationtoolApplication.class, args);
+	}
+}
---
+++ src/test/java/com/appslabtest/ValidationtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appslabtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ValidationtoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
